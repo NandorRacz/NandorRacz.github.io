7195920fe3bbadd81f6f054f0c88aa2bae686684
NandorRacz <33574298+NandorRacz@users.noreply.github.com>
2022-11-10 20:12:02 +0100

    switched to burg menus

--- js/bella.js
+++ js/bella.js
@@ -1,73 +1,77 @@
 $(document).ready(function(){
   console.log("bella page loaded");
   $(".bella-info").hide();
-  $(".bella-posts").hide();
+  $(".bella-events").hide();
   $(".bella-reviews").hide();
-  $(".bella-history").hide();
+  $(".bella-discounts").hide();
   $("#menu").addClass("current-tab");
+
+  $("#bella-nav-button").click(function() {
+    $(".bella-nav-list").toggleClass('bella-nav-list-show');
+  });
 
   $("#menu").click(function(){
     $(".bella-menu").show();
     $(".bella-info").hide();
-    $(".bella-posts").hide();
+    $(".bella-events").hide();
     $(".bella-reviews").hide();
-    $(".bella-history").hide();
+    $(".bella-discounts").hide();
     $("#menu").addClass("current-tab");
     $("#info").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#events").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
-    $("#history").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
   });
 
   $("#info").click(function(){
     $(".bella-info").show();
     $(".bella-menu").hide();
-    $(".bella-posts").hide();
+    $(".bella-events").hide();
     $(".bella-reviews").hide();
-    $(".bella-history").hide();
+    $(".bella-discounts").hide();
     $("#info").addClass("current-tab");
     $("#menu").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#events").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
-    $("#history").removeClass("current-tab");
-    $("#history").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
   });
 
-  $("#posts").click(function(){
-    $(".bella-posts").show();
+  $("#events").click(function(){
+    $(".bella-events").show();
     $(".bella-info").hide();
     $(".bella-menu").hide();
     $(".bella-reviews").hide();
-    $(".bella-history").hide();
-    $("#posts").addClass("current-tab");
+    $(".bella-discounts").hide();
+    $("#events").addClass("current-tab");
     $("#info").removeClass("current-tab");
     $("#menu").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
-    $("#history").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
   });
 
   $("#reviews").click(function(){
     $(".bella-reviews").show();
     $(".bella-info").hide();
     $(".bella-menu").hide();
-    $(".bella-posts").hide();
-    $(".bella-history").hide();
+    $(".bella-events").hide();
+    $(".bella-discounts").hide();
     $("#reviews").addClass("current-tab");
     $("#info").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#events").removeClass("current-tab");
     $("#menu").removeClass("current-tab");
-    $("#history").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
   });
 
-  $("#history").click(function(){
-    $(".bella-history").show();
+  $("#discounts").click(function(){
+    $(".bella-discounts").show();
     $(".bella-info").hide();
-    $(".bella-posts").hide();
+    $(".bella-events").hide();
     $(".bella-reviews").hide();
     $(".bella-menu").hide();
-    $("#history").addClass("current-tab");
+    $("#discounts").addClass("current-tab");
     $("#info").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#events").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
     $("#menu").removeClass("current-tab");
   });
--- js/cafe.js
+++ js/cafe.js
@@ -1,75 +1,84 @@
 $(document).ready(function(){
   console.log("Cafe page loaded");
   $(".cafe-info").hide();
-  $(".cafe-posts").hide();
+  $(".cafe-discounts").hide();
   $(".cafe-reviews").hide();
   $(".cafe-events").hide();
   $("#menu").addClass("current-tab");
 
+  $("#cafe-nav-button").click(function() {
+    $(".cafe-nav-list").toggleClass('cafe-nav-list-show');
+  });
+
   $("#menu").click(function(){
     $(".cafe-menu").show();
     $(".cafe-info").hide();
-    $(".cafe-posts").hide();
+    $(".cafe-discounts").hide();
     $(".cafe-reviews").hide();
     $(".cafe-events").hide();
     $("#menu").addClass("current-tab");
     $("#info").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
     $("#events").removeClass("current-tab");
+    $(".cafe-nav-list").removeClass('cafe-nav-list-show');
   });
 
   $("#info").click(function(){
     $(".cafe-info").show();
     $(".cafe-menu").hide();
-    $(".cafe-posts").hide();
+    $(".cafe-discounts").hide();
     $(".cafe-reviews").hide();
     $(".cafe-events").hide();
     $("#info").addClass("current-tab");
     $("#menu").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
     $("#events").removeClass("current-tab");
     $("#events").removeClass("current-tab");
+    $(".cafe-nav-list").removeClass('cafe-nav-list-show');
   });
 
-  $("#posts").click(function(){
-    $(".cafe-posts").show();
+  $("#discounts").click(function(){
+    $(".cafe-discounts").show();
     $(".cafe-info").hide();
     $(".cafe-menu").hide();
     $(".cafe-reviews").hide();
     $(".cafe-events").hide();
-    $("#posts").addClass("current-tab");
+    $("#discounts").addClass("current-tab");
     $("#info").removeClass("current-tab");
     $("#menu").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
     $("#events").removeClass("current-tab");
+    $(".cafe-nav-list").removeClass('cafe-nav-list-show');
   });
 
   $("#reviews").click(function(){
     $(".cafe-reviews").show();
     $(".cafe-info").hide();
     $(".cafe-menu").hide();
-    $(".cafe-posts").hide();
+    $(".cafe-discounts").hide();
     $(".cafe-events").hide();
     $("#reviews").addClass("current-tab");
     $("#info").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
     $("#menu").removeClass("current-tab");
     $("#events").removeClass("current-tab");
+    $(".cafe-nav-list").removeClass('cafe-nav-list-show');
   });
 
   $("#events").click(function(){
     $(".cafe-events").show();
     $(".cafe-info").hide();
-    $(".cafe-posts").hide();
+    $(".cafe-discounts").hide();
     $(".cafe-reviews").hide();
     $(".cafe-menu").hide();
     $("#events").addClass("current-tab");
     $("#info").removeClass("current-tab");
-    $("#posts").removeClass("current-tab");
+    $("#discounts").removeClass("current-tab");
     $("#reviews").removeClass("current-tab");
     $("#menu").removeClass("current-tab");
+    $(".cafe-nav-list").removeClass('cafe-nav-list-show');
   });
 
 });
